30623810872347d4c058e0721daa3073af2cfd5d
BORGES <jessborgesb@gmail.com>
2026-04-25 14:54:52 +0200

    feat: add Controller, Service, Respository - POST /fruits returns 201 (green)

---
+++ src/main/java/cat/itacademy/s04/t02/n01/fruit_api_h2/controller/FruitController.java
@@ -0,0 +1,27 @@
+package cat.itacademy.s04.t02.n01.fruit_api_h2.controller;
+
+import cat.itacademy.s04.t02.n01.fruit_api_h2.dto.FruitRequestDto;
+import cat.itacademy.s04.t02.n01.fruit_api_h2.dto.FruitResponseDto;
+import cat.itacademy.s04.t02.n01.fruit_api_h2.service.FruitService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fruits")
+@RequiredArgsConstructor
+public class FruitController {
+
+    private final FruitService fruitService;
+
+    @PostMapping
+    public ResponseEntity<FruitResponseDto> createFruit(@Valid @RequestBody FruitRequestDto dto) {
+        FruitResponseDto response = fruitService.createFruit(dto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+}
