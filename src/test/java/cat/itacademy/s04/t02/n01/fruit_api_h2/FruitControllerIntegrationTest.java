7ddd6b5710a25add29a6a523af24f1aa43ee436d
BORGES <jessborgesb@gmail.com>
2026-04-25 15:24:07 +0200

    test: add GET /fruits integration test (red)

--- src/test/java/cat/itacademy/s04/t02/n01/fruit_api_h2/FruitControllerIntegrationTest.java
+++ src/test/java/cat/itacademy/s04/t02/n01/fruit_api_h2/FruitControllerIntegrationTest.java
@@ -1,5 +1,6 @@
 package cat.itacademy.s04.t02.n01.fruit_api_h2;
 
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,13 +8,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 class FruitControllerIntegrationTest {
 
     @Autowired
@@ -21,7 +25,7 @@ class FruitControllerIntegrationTest {
 
     @Test
     @DisplayName("Should return 201 Created and the persisted fruit when a valid POST request is made")
-    public void createFruit_withValidData_returns201WithFruit() throws Exception {
+    void createFruit_withValidData_returns201WithFruit() throws Exception {
 
         String fruitJson = "{\"name\": \"Apple\", \"weightInKilos\": 3}";
 
@@ -36,7 +40,7 @@ class FruitControllerIntegrationTest {
 
     @Test
     @DisplayName("Should return 400 when name is blank")
-    public void createFruit_withInvalidName_returns400() throws Exception {
+    void createFruit_withInvalidName_returns400() throws Exception {
 
         String fruitJson = "{\"name\": \"\", \"weightInKilos\": 10}";
 
@@ -48,7 +52,7 @@ class FruitControllerIntegrationTest {
 
     @Test
     @DisplayName("Should return 400 when weight is zero")
-    public void createFruit_withInvalidWeight_returns400() throws Exception {
+    void createFruit_withInvalidWeight_returns400() throws Exception {
 
         String fruitJson = "{\"name\": \"Apple\", \"weightInKilos\": 0}";
 
@@ -60,12 +64,35 @@ class FruitControllerIntegrationTest {
 
     @Test
     @DisplayName("Should return 400 when weight is a negative")
-    public void createFruit_withNegativeWeight_returns400() throws Exception {
+    void createFruit_withNegativeWeight_returns400() throws Exception {
         String fruitJson = "{\"name\": \"Apple\", \"weightInKilos\": -1}";
 
         mockMvc.perform(post("/fruits")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(fruitJson))
                 .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Should return a list of fruits and 200 OK")
+    void getAllFruits_withValidData_returnsListOfFruits() throws Exception {
+
+        String fruit1 = "{\"name\": \"Banana\", \"weightInKilos\": 3}";
+        String fruit2 = "{\"name\": \"Orange\", \"weightInKilos\": 4}";
+
+        mockMvc.perform(post("/fruits")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(fruit1));
+
+        mockMvc.perform(post("/fruits")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(fruit2));
+
+        mockMvc.perform(get("/fruits")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].name").value("Banana"))
+                .andExpect(jsonPath("$[1].name").value("Orange"));
     }
 }
