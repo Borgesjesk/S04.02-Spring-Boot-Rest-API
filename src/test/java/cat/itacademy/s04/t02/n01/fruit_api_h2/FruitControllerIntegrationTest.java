37ec8fb5b7b316856b961cce216d004688aa99dd
BORGES <jessborgesb@gmail.com>
2026-04-25 15:06:51 +0200

    test: add validation test for POST /fruits (blank name, zero/negative weight)

--- src/test/java/cat/itacademy/s04/t02/n01/fruit_api_h2/FruitControllerIntegrationTest.java
+++ src/test/java/cat/itacademy/s04/t02/n01/fruit_api_h2/FruitControllerIntegrationTest.java
@@ -33,4 +33,39 @@ class FruitControllerIntegrationTest {
                 .andExpect(jsonPath("$.name").value("Apple"))
                 .andExpect(jsonPath("$.weightInKilos").value(3));
     }
+
+    @Test
+    @DisplayName("Should return 400 when name is blank")
+    public void createFruit_withInvalidName_returns400() throws Exception {
+
+        String fruitJson = "{\"name\": \"\", \"weightInKilos\": 10}";
+
+        mockMvc.perform(post("/fruits")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(fruitJson))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Should return 400 when weight is zero")
+    public void createFruit_withInvalidWeight_returns400() throws Exception {
+
+        String fruitJson = "{\"name\": \"Apple\", \"weightInKilos\": 0}";
+
+        mockMvc.perform(post("/fruits")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(fruitJson))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Should return 400 when weight is a negative")
+    public void createFruit_withNegativeWeight_returns400() throws Exception {
+        String fruitJson = "{\"name\": \"Apple\", \"weightInKilos\": -1}";
+
+        mockMvc.perform(post("/fruits")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(fruitJson))
+                .andExpect(status().isBadRequest());
+    }
 }
