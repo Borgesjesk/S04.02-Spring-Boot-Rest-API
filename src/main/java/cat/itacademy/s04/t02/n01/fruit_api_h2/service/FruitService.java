6ae26e9bf40f62058cfb94ca21295c6bc074aaf1
BORGES <jessborgesb@gmail.com>
2026-04-25 14:46:49 +0200

    feat: add FruitService interface and implementation with created method

---
+++ src/main/java/cat/itacademy/s04/t02/n01/fruit_api_h2/service/FruitService.java
@@ -0,0 +1,8 @@
+package cat.itacademy.s04.t02.n01.fruit_api_h2.service;
+
+import cat.itacademy.s04.t02.n01.fruit_api_h2.dto.FruitRequestDto;
+import cat.itacademy.s04.t02.n01.fruit_api_h2.dto.FruitResponseDto;
+
+public interface FruitService {
+    FruitResponseDto createFruit(FruitRequestDto dto);
+}
---
+++ src/main/java/cat/itacademy/s04/t02/n01/fruit_api_h2/service/FruitServiceImpl.java
@@ -0,0 +1,25 @@
+package cat.itacademy.s04.t02.n01.fruit_api_h2.service;
+
+import cat.itacademy.s04.t02.n01.fruit_api_h2.dto.FruitRequestDto;
+import cat.itacademy.s04.t02.n01.fruit_api_h2.dto.FruitResponseDto;
+import cat.itacademy.s04.t02.n01.fruit_api_h2.mapper.FruitMapper;
+import cat.itacademy.s04.t02.n01.fruit_api_h2.model.Fruit;
+import cat.itacademy.s04.t02.n01.fruit_api_h2.repository.FruitRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FruitServiceImpl implements FruitService {
+
+    private final FruitRepository fruitRepository;
+
+    @Override
+    public FruitResponseDto createFruit(FruitRequestDto fruitRequestDto) {
+        Fruit fruit = FruitMapper.toEntity(fruitRequestDto);
+
+        Fruit savedFruit = fruitRepository.save(fruit);
+
+        return FruitMapper.toResponseDto(savedFruit);
+    }
+}
